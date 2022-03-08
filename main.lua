9809925b3f61cb73a32b8d3d6cbf9ff9b5b4fca4
garfieldcatto <71535863+2dgeneralspam1@users.noreply.github.com>
2022-03-07 20:26:01 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,10 @@
 setclipboard('https://raw.githubusercontent.com/2dgeneralspam1/CheatX/main/misc/basic.lua')
 
-while wait() do 
+
     game.StarterGui:SetCore("SendNotification", {
 	Title = "Garfield"; 
 	Text = "NEW SCRIPT COPIED TO YOUR CLIPBOARD, PLEASE USE THAT SCRIPT INSTEAD!!! (COPY LINK TO BROWSER THEN COPY THE CODE BLOCK)";
 	Icon = ""; 
-	Duration = 5; 
+	Duration = math.huge; 
 })
 
-end 
